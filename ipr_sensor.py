28739d862bed81fa9a6efec57d7aac4a161bb820
phil-engineering <111706453+phil-engineering@users.noreply.github.com>
2025-12-09 12:58:33 -0800

    Add more items to menu

--- ipr_sensor.py
+++ ipr_sensor.py
@@ -1,7 +1,6 @@
 import queue
 import time
 
-# from ipr_sensor_serial_logger import *
 from ipr_sensor_command import IprSensorCommand
 from ipr_sensor_serial import IprSensorSerial
 from ipr_sensor_logging import IprSensorSerialLoggerThread
@@ -29,16 +28,20 @@ time.sleep(0.5)
 def main():
     # Main loop - process commands
     while True:
+        # Wait for user input command
         user_cmd = input("> ")
 
         if user_cmd == "menu" or user_cmd == "?":
             print("Available commands:")
             print("[menu] or [?]: Access this menu")
             print("[init]: List the sensor information, name, and time")
+            print("[get_material]: Display the sensor's material")
+            print("[set_material]: Change the sensor's material")
+            print("[get_name]: Display the sensor's name")
+            print("[set_name]: Change the sensor's name")
+            print("[set_tare]: Apply a new tare")
             print("[get_time]: Display the sensor's internal time")
             print("[set_time]: Change the sensor's internal time")
-            print("[get_name]: Display the sensor's name")
-            print("[set_tare]: Apply a new tare")
             print("[start_recording]: Start recording sensor data to a BIN file")
             print("[stop_recording]: Stop the sensor's recording")
 
@@ -52,25 +55,25 @@ def main():
         elif user_cmd == "get_name":
             if logger.is_logging():
                 logger.stop_logging()
-                # ipr_cmd.stop_sensor_transmit()
             print(ipr_cmd.get_name())
+
+        elif user_cmd == "set_name":
+            if logger.is_logging():
+                logger.stop_logging()
 
         elif user_cmd == "get_time":
             if logger.is_logging():
                 logger.stop_logging()
-                # ipr_cmd.stop_sensor_transmit()
             print(ipr_cmd.get_time())
 
         elif user_cmd == "set_time":
             if logger.is_logging():
                 logger.stop_logging()
-                ipr_cmd.stop_sensor_transmit()
             ipr_cmd.set_time_interactive()
 
         elif user_cmd == "set_tare":
             if logger.is_logging():
                 logger.stop_logging()
-                ipr_cmd.stop_sensor_transmit()
             ipr_cmd.set_tare()
 
         elif user_cmd == "start_recording":
